2af73fceef3b2fe1ac6f3ab815cac9799f15571f
Carlos Panganiban <cgpanganiban@up.edu.ph>
2025-11-18 11:10:47 +0800

    Add DebugPrinter

--- start.bat
+++ start.bat
@@ -1,11 +1,14 @@
 @echo off
 mkdir "%localappdata%\Whiskerwood\Saved\mods\FaithfulCopperSlides" 2>nul
-mkdir "%localappdata%\Whiskerwood\Saved\mods\DumpDataTables" 2>nul
 mkdir "%localappdata%\Whiskerwood\Saved\mods\WhiskersNewClothes" 2>nul
+
+mkdir "%localappdata%\Whiskerwood\Saved\mods\DumpDataTables" 2>nul
+mkdir "%localappdata%\Whiskerwood\Saved\mods\DebugPrinter" 2>nul
 
 copy /Y "C:\dev\whiskerwood-mods\Windows\Whiskerwood\Content\Paks\pakchunk250-Windows.pak" "%localappdata%\Whiskerwood\Saved\mods\FaithfulCopperSlides\FaithfulCopperSlides.pak"
 copy /Y "C:\dev\whiskerwood-mods\Windows\Whiskerwood\Content\Paks\pakchunk252-Windows.pak" "%localappdata%\Whiskerwood\Saved\mods\WhiskersNewClothes\WhiskersNewClothes.pak"
 
+copy /Y "C:\dev\whiskerwood-mods\Windows\Whiskerwood\Content\Paks\pakchunk299-Windows.pak" "%localappdata%\Whiskerwood\Saved\mods\DebugPrinter\DebugPrinter.pak"
 copy /Y "C:\dev\whiskerwood-mods\Windows\Whiskerwood\Content\Paks\pakchunk16-Windows.pak" "%localappdata%\Whiskerwood\Saved\mods\DumpDataTables\DumpDataTables.pak"
 
 copy /Y "C:\dev\whiskerwood-mods\uplugin_files\FaithfulCopperSlides.uplugin" "%localappdata%\Whiskerwood\Saved\mods\FaithfulCopperSlides\FaithfulCopperSlides.uplugin"
